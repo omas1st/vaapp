f3025fc5f1f6892b916fad1fb7fce851ab38bff8
omas1st <omas1st@gmail.com>
2025-04-06 03:14:16 +0100

    Update Application.js

--- model/Application.js
+++ model/Application.js
@@ -1,16 +1,24 @@
 const mongoose = require('mongoose');
 
 const applicationSchema = new mongoose.Schema({
-  destination: String,
-  visaType: String,
-  citizenship: String,
-  email: String,
-  phone: String,
-  firstName: String,
-  lastName: String,
-  paymentMethod: String,
-  receiptPath: String,
-  createdAt: { type: Date, default: Date.now }
+    destination: { type: String, required: true },
+    visaType: { type: String, required: true },
+    citizenship: { type: String, required: true },
+    email: { type: String, validate: /^\S+@\S+\.\S+$/ },
+    phone: String,
+    passportCitizenship: String,
+    firstName: String,
+    lastName: String,
+    paymentMethod: String,
+    receiptPath: String,
+    status: { type: String, default: 'incomplete' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+applicationSchema.pre('save', function(next) {
+    this.updatedAt = new Date();
+    next();
 });
 
 module.exports = mongoose.model('Application', applicationSchema);
